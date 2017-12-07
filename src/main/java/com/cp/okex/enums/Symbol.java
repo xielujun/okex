dffa7c66835370577e442126fefcec45b4a44a7b
ex-chengp <ex-chengp@T-42CQF52.cmfdev.cmfchina.com>
2017-12-07 18:59:36 +0800

    frist submit!

---
+++ src/main/java/com/cp/okex/enums/Symbol.java
@@ -0,0 +1,25 @@
+package com.cp.okex.enums;
+
+public enum Symbol {
+	
+	/**
+	 * 比特币
+	 */
+	btc_usd,
+	/**
+	 * 莱特币
+	 */
+	ltc_usd,
+	/**
+	 * 以太坊
+	 */
+	eth_usd,
+	/**
+	 * 以太坊经典
+	 */
+	etc_usd,
+	/**
+	 * 比特币现金
+	 */
+	bch_usd;
+}

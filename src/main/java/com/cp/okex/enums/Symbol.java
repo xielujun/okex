a671ed54c50e122cb19ec1d26a29e12548497e31
ex-chengp <ex-chengp@T-42CQF52.cmfdev.cmfchina.com>
2017-12-07 21:42:53 +0800

    test okhttp

--- src/main/java/com/cp/okex/enums/Symbol.java
+++ src/main/java/com/cp/okex/enums/Symbol.java
@@ -1,25 +1,35 @@
 package com.cp.okex.enums;
 
+/**
+ * 币种标识
+ * @author ex-chengp
+ *
+ */
 public enum Symbol {
 	
 	/**
 	 * 比特币
 	 */
 	btc_usd,
+	
 	/**
 	 * 莱特币
 	 */
 	ltc_usd,
+	
 	/**
 	 * 以太坊
 	 */
 	eth_usd,
+	
 	/**
 	 * 以太坊经典
 	 */
 	etc_usd,
+	
 	/**
 	 * 比特币现金
 	 */
 	bch_usd;
+	
 }
